4c7d77adbebad80fc4ccbddb265d3db67653c328
anjuchamantha <anjuchamantha@users.noreply.github.com>
2026-09-18 11:43:44 +0000

    deploy: 047e5229fe4b991fd38a9d1f4212f11b23f8b03b

---
+++ assets/js/5e95c892.d9f6ddcd.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkdpdp_accelerator_docs||=[]).push([[647],{7121(e,r,s){s.r(r),s.d(r,{default:()=>o});s(6540);var a=s(4164),c=s(7559),d=s(5500),t=s(2831),u=s(4060),l=s(4848);function o(e){return(0,l.jsx)(d.e3,{className:(0,a.A)(c.G.wrapper.docsPages),children:(0,l.jsx)(u.A,{children:(0,t.v)(e.route.routes)})})}}}]);
